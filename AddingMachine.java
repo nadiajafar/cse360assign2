6987d397f3ecb39e7fe949176bfd778a1db07148
nadiajafar <44142910+nadiajafar@users.noreply.github.com>
2019-10-08 21:24:42 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,39 @@
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine()
+	{
+		total=0;
+		//for clarity
+	}
+	
+	public int getTotal()
+	{
+		return 0 ;
+	}
+	
+	public void add(int value)
+	{
+		
+	}
+	
+	public void subtract(int value)
+	{
+		
+	}
+	
+	public String toString()
+	{
+		return "";
+		
+	}
+	
+	public void clear()
+	{
+		
+	}
+
+}
